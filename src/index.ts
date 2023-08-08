13ae367594bc6344fee55343746e0e4a0f7ee13c
The0Show <62229104+The0Show@users.noreply.github.com>
2023-08-08 01:05:14 -0400

    a

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
 	res.sendFile(`${__dirname}/StayInsideMe.mp3`);
 });
 
+app.get("*", (req, res) => {
+	res.send("uhm i don't think you get that this is a test >:(");
+});
+
 app.listen(3000, () => {
 	console.log("we're live");
 });
